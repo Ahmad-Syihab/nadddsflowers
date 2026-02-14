34beb2b73c7a7a2189f227b76344409107202f66
Ahmad-Syihab <165917824+Ahmad-Syihab@users.noreply.github.com>
2026-02-14 13:05:44 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Yeay! kamu dapetin website inii:D`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
