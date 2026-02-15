df2fc470869f129c3de9787702baa247bea66d3f
Ahmad-Syihab <165917824+Ahmad-Syihab@users.noreply.github.com>
2026-02-15 13:50:55 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Bunga hidup bisa layu, Bunga plastik bisa hilang... maka aku beri bunga ini untukmu tidak akan hilang dan tidak akan mati.. dia abadi selama bumi ini masi ada').split('')
+    const titles = ('Bunga ini abadi bersama memori kita').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
